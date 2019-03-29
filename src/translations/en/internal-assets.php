ea664c49e988f7513738880a2aac04d04e46a492
Ramon Saccilotto <tikiatua@gmail.com>
2019-03-29 07:44:03 +0100

    add english translation for plugin info

--- src/translations/en/internal-assets.php
+++ src/translations/en/internal-assets.php
@@ -5,5 +5,5 @@ return [
     'Internal Assets plugin loaded' => 'Internal Assets plugin loaded',
     'volume-name' => 'Local folder (protected)',
     'file-path-info' => 'The path to the volume’s directory on the file system. An absolute path ({ex1}) is recommended. It should be located outside of your web root or protected against direct web access.',
-    'plugin-info' => 'Aktivieren Sie öffentliche URLs für dieses Volume, damit Benutzer mit entsprechenden Berechtigungen über das Frontend auf Dateien zugreifen können. Alle Urls werden automatisch mit dem Präfix "/internal" versehen, damit die Berechtigungen beim Zugriff überprüft werden können.',
+    'plugin-info' => 'Enable public URLs for this volume so that users with appropriate permissions can access files through the frontend. All urls are automatically prefixed with "/internal" so that permissions can be checked on access.',
 ];
